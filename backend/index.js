a3037177f65be74882ba01bb553148eac3cab35c
Ayush Kumar Jha <88491971+ayush2024@users.noreply.github.com>
2024-02-22 21:25:31 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(passport.session());
 
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "https://react-oath-front.vercel.app",
         methods: "GET,POST,PUT,DELETE",
         credentials: true,
     })
