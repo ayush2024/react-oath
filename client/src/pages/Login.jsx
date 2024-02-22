834b57f7f65a3b4b9356e5ccb423c30567002425
Ayush Kumar Jha <88491971+ayush2024@users.noreply.github.com>
2024-02-22 21:21:40 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,15 +4,15 @@ import Github from "../img/github.png";
 
 const Login = () => {
   const google = () => {
-    window.open("http://localhost:8000/auth/google", "_self");
+    window.open("https://react-oath-api.vercel.app/google", "_self");
   };
 
   const github = () => {
-    window.open("http://localhost:8000/auth/github", "_self");
+    window.open("https://react-oath-api.vercel.app/github", "_self");
   };
 
   const facebook = () => {
-    window.open("http://localhost:8000/auth/facebook", "_self");
+    window.open("https://react-oath-api.vercel.app/facebook", "_self");
   };
 
   return (
